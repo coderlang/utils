d19285d567a0399334aab49e7898c5f2cab96632
coderlang <coderlang@foxmail.com>
2020-12-15 14:12:09 +0800

    修改init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-git submodule foreach git submodule init
-git submodule foreach git submodule update
-git submodule foreach git checkout master
+git submodule init
+git submodule update
+git checkout master
