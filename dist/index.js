dea641c47cade7294ea57944ae45b88717604b81
coderlang <coderlang@foxmail.com>
2020-12-25 16:52:45 +0800

    删除dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-export { ClassArray } from "./src/ClassArray";
-export * from "./src/Json";
-export { IdNumber, IdNumberGender } from "./src/IdNumber";
-export * from "./src/Sign";
-export * from "./src/fun";
-export * from "./src/duration";
--- dist/src/condition/AndCondition.js
+++
@@ -1,9 +0,0 @@
-import { LogicCondition } from "./LogicCondition";
-export class AndCondition extends LogicCondition {
-    getOp() {
-        throw "and";
-    }
-    isAndCondition() {
-        return true;
-    }
-}
