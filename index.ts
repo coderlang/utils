27ef1c27109166abfa865db8854b80f28f6cc6aa
coderlang <coderlang@foxmail.com>
2021-02-02 17:23:33 +0800

    移除没必要代码

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 export {ClassArray} from "./src/ClassArray";
 export * from "./src/Json";
-export {IdNumber, IdNumberGender} from "./src/IdNumber";
-export * from "./src/Sign";
 export * from "./src/fun";
 export * from "./src/duration"
 export {Class} from "./src/Class";
